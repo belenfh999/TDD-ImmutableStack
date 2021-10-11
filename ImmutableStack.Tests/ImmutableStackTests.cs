96c9ce057aae05f31bde0b248b56605b8986072b
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-11 14:55:41 +0200

    addded test for popping two items

--- ImmutableStack.Tests/ImmutableStackTests.cs
+++ ImmutableStack.Tests/ImmutableStackTests.cs
@@ -22,8 +22,34 @@ namespace ImmutableStack.Tests
             Assert.AreEqual(0, previousStack.Count);
             Assert.AreEqual(0, emptyStack.Count);
             Assert.AreEqual(1, modifiedStack.Count);
-
-
         }
+
+        [TestMethod]
+        public void canPopOff2Items()
+        {
+            var previousStack = new ImmutableStack<string>();
+            var modifiedStack = previousStack.Push("foo");
+            var modifiedStack2 = modifiedStack.Push("bar");
+            Assert.AreEqual(0, previousStack.Count);
+            Assert.AreEqual(1, modifiedStack.Count);
+            Assert.AreEqual(2, modifiedStack2.Count);
+
+            Assert.AreEqual("foo", modifiedStack.Peek());
+            Assert.AreEqual("foo", modifiedStack.Peek());
+            Assert.AreEqual("bar", modifiedStack2.Peek());
+            Assert.AreEqual("bar", modifiedStack2.Peek());
+
+            var emptyStack = modifiedStack.Pop();
+            Assert.AreEqual(0, previousStack.Count);
+            Assert.AreEqual(0, emptyStack.Count);
+            Assert.AreEqual(1, modifiedStack.Count);
+
+            var stack1Item = modifiedStack2.Pop();
+            Assert.AreEqual(0, previousStack.Count);
+            Assert.AreEqual(1, modifiedStack.Count);
+            Assert.AreEqual(2, modifiedStack2.Count);
+            Assert.AreEqual(1, stack1Item.Count);
+        }
+        
     }
 }
