0c9f1d178f06410492be8a3111b54d343fd58a6f
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-11 14:50:15 +0200

    Add failing test for popping one item

--- ImmutableStack.Tests/ImmutableStackTests.cs
+++ ImmutableStack.Tests/ImmutableStackTests.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ImmutableStack.Library;
 
 namespace ImmutableStack.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class ImmutableStacksTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void canPopOff1Item()
         {
+            var previousStack = new ImmutableStack<string>();
+            var modifiedStack = previousStack.Push("foo");
+            Assert.AreEqual(0, previousStack.Count);
+            Assert.AreEqual(1, modifiedStack.Count);
+
+            Assert.AreEqual("foo", modifiedStack.Peek());
+            Assert.AreEqual("foo", modifiedStack.Peek());
+
+            var emptyStack = modifiedStack.Pop();
+            Assert.AreEqual(0, previousStack.Count);
+            Assert.AreEqual(0, emptyStack.Count);
+            Assert.AreEqual(1, modifiedStack.Count);
+
+
         }
     }
 }
