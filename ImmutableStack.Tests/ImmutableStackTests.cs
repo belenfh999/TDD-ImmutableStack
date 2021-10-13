e1161bef78dc4cc1b47293678084e3b5937fef83
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-13 09:10:01 +0200

    Rollback to make tests simpler

--- ImmutableStack.Tests/ImmutableStackTests.cs
+++ ImmutableStack.Tests/ImmutableStackTests.cs
@@ -8,54 +8,13 @@ namespace ImmutableStack.Tests
     public class ImmutableStacksTests
     {
         [TestMethod]
-        public void canPopOff1Item()
+        public void canPush1Item()
         {
             var previousStack = new ImmutableStack<string>();
             var modifiedStack = previousStack.Push("foo");
             Assert.AreEqual(0, previousStack.Count);
             Assert.AreEqual(1, modifiedStack.Count);
-
-            Assert.AreEqual("foo", modifiedStack.Peek());
-            Assert.AreEqual("foo", modifiedStack.Peek());
-
-            var emptyStack = modifiedStack.Pop();
-            Assert.AreEqual(0, previousStack.Count);
-            Assert.AreEqual(0, emptyStack.Count);
-            Assert.AreEqual(1, modifiedStack.Count);
         }
 
-        [TestMethod]
-        public void canPopOff2Items()
-        {
-            var previousStack = new ImmutableStack<string>();
-            var modifiedStack = previousStack.Push("foo");
-            var modifiedStack2 = modifiedStack.Push("bar");
-            Assert.AreEqual(0, previousStack.Count);
-            Assert.AreEqual(1, modifiedStack.Count);
-            Assert.AreEqual(2, modifiedStack2.Count);
-
-            Assert.AreEqual("foo", modifiedStack.Peek());
-            Assert.AreEqual("foo", modifiedStack.Peek());
-            Assert.AreEqual("bar", modifiedStack2.Peek());
-            Assert.AreEqual("bar", modifiedStack2.Peek());
-
-            var emptyStack = modifiedStack.Pop();
-            Assert.AreEqual(0, previousStack.Count);
-            Assert.AreEqual(0, emptyStack.Count);
-            Assert.AreEqual(1, modifiedStack.Count);
-
-            var stack1Item = modifiedStack2.Pop();
-            Assert.AreEqual(0, previousStack.Count);
-            Assert.AreEqual(1, modifiedStack.Count);
-            Assert.AreEqual(2, modifiedStack2.Count);
-            Assert.AreEqual(1, stack1Item.Count);
-        }
-
-        [TestMethod]
-        public void CanPopFromEmptyStack()
-        {
-            var emptyStack = new ImmutableStack<string>();
-            Assert.AreEqual(default(string), emptyStack.Pop());
-        }
     }
 }
